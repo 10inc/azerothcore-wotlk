f3c85a0af54384c51ab0a9d3e65e8522eed23366
Ethan Sonza <ethanjericho@yahoo.com>
2023-02-08 23:12:54 +1000

    Discord bot status update

--- discord-bot/commands/status.js
+++ discord-bot/commands/status.js
@@ -1,20 +1,26 @@
 require('dotenv').config()
 const { SlashCommandBuilder } = require('discord.js')
-const { connection } = require('../helpers/conn')
+const { connectionChar } = require("../helpers/conn");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('status')
     .setDescription('Player count in HT WoW'),
   async execute (interaction) {
-    connection.query('select count(online) from account where online = 1', (error, results, fields) => {
-      if (error) {
-        console.error(error)
-        return interaction.reply('Something went wrong. Ask @MGMT for help.')
+    connectionChar.query(
+      "SELECT name FROM characters WHERE online = 1",
+      (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          return interaction.reply("Something went wrong. Ask @MGMT for help.");
+        }
+        const count = results.length();
+        const names = results.join(",");
+        console.log(results);
+        console.log(names);
+        console.log(count);
+        return interaction.reply(`${count} Players online: ${names}`);
       }
-      return interaction.reply(
-        `Players online: ${Object.values(results[0])[0]}`
-      )
-    })
+    );
   }
 }
--- discord-bot/helpers/conn.js
+++ discord-bot/helpers/conn.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection({
+
+DB_PARAMS = {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: "acore_auth",
-});
+}
+
+const connection = mysql.createConnection({ ...DB_PARAMS, database: "acore_auth" });
+const connectionChar = mysql.createConnection({ ...DB_PARAMS, database: "acore_characters" });
 
 module.exports = {
-  connection: connection
-}
+  connection: connection,
+  connectionChar: connectionChar,
+};
